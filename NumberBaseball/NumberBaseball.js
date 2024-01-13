1d8fff677740a8c0069b1cb8616c9cedf8293b9b
smg0725 <smg0725@naver.com>
2024-01-14 02:32:55 +0900

    NumberBaseBall1

---
+++ NumberBaseball/NumberBaseball.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+
+class NumberBaseball extends Component{
+
+}
+
+export  const hello = 'hello'; //import { hello }
+export const  bye = 'hello'; // import { hello,bye }
+export default NumberBaseball; // import NumberBaseball
+//export를 할 때 const default의 경우 그냥 import NumberBaseball을 진행하고
+// hello와 bye 처럼 각각 이름이 정해져 있으면 import {hello}, import {bye}로 import를 진행한다.
---
+++ NumberBaseball/client.jsx
@@ -0,0 +1,7 @@
+const React = require("react");
+const ReactDom = require("react-dom");
+const { hot } = require("react-hot-loader/root");
+
+const NumberBaseball = require("./NumberBaseball");
+
+ReactDom.createRoot().render(<NumberBaseball></NumberBaseball>);
